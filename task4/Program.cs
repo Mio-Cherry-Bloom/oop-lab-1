aa7c8a95681495f5079ac865943869d2cf893658
Mio <savaluchian1@gmail.com>
2024-10-29 18:30:40 +0200

    Task4 implementation

---
+++ task4/Program.cs
@@ -0,0 +1,22 @@
+﻿
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        FileReader fileReader = new FileReader();
+        foreach (string filePath in args)
+        {
+            try
+            {
+                string textContent = fileReader.ReadFileIntoString(filePath);
+                TextData textData = new TextData(filePath, textContent);
+                Console.WriteLine(textData);
+                Console.WriteLine("\n \n"); 
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error reading file '{filePath}': {ex.Message}");
+            }
+        }
+    }
+}
